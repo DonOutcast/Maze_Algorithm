8afc4a8e3352ca9751f5193944ff44a33addbd66
DonOutcast <lymondgl@student.21-school.ru>
2023-02-13 19:27:49 +0300

    maze_scene in progress

---
+++ src/maze_view/maze_scene.cpp
@@ -0,0 +1,6 @@
+#include "maze_scene.h"
+
+maze_scene::maze_scene()
+{
+
+}
---
+++ src/maze_view/maze_scene.h
@@ -0,0 +1,11 @@
+#ifndef MAZE_SCENE_H
+#define MAZE_SCENE_H
+
+
+class maze_scene
+{
+public:
+    maze_scene();
+};
+
+#endif // MAZE_SCENE_H
